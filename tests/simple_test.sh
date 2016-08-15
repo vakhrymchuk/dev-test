65f4ba94e1a481f1ee2095ce321b0b98a0db4cc7
Florian Merz <flomerz@gmail.com>
2016-08-15 08:17:29 +0200

    Fix URL in test

--- tests/simple_test.sh
+++ tests/simple_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sleep 5
 {
-    curl -s localhost:8088/API/providers/bus/goeurobus/direct/3/4 | grep -E 'true|false' > /dev/null &&
-    curl -s localhost:8088/API/providers/bus/goeurobus/direct/0/1 | grep -E 'true|false' > /dev/null &&
+    curl -s localhost:8088/rest/provider/goeurobus/direct/3/4 | grep -E 'true|false' > /dev/null &&
+    curl -s localhost:8088/rest/provider/goeurobus/direct/0/1 | grep -E 'true|false' > /dev/null &&
     echo TEST PASSED!
 } || {
     echo TEST FAILED!
