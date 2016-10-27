08630bb42a7be021185b2af0aae6c84cbbfc5dff
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2016-10-27 21:30:51 +0300

    Change context path to /api

--- src/main/java/com/github/vakhrymchuk/rest/BusRouteController.java
+++ src/main/java/com/github/vakhrymchuk/rest/BusRouteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/direct")
+@RestController("/direct")
 public class BusRouteController {
 
     @Autowired
