bd0e1d19bbe483bb7a219ea3e6cb949a3cc5fc56
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2016-10-27 22:22:17 +0300

    Add logger to service

--- src/main/java/com/github/vakhrymchuk/service/BusRouteServiceImpl.java
+++ src/main/java/com/github/vakhrymchuk/service/BusRouteServiceImpl.java
@@ -1,13 +1,20 @@
 package com.github.vakhrymchuk.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @Service
 public class BusRouteServiceImpl implements BusRouteService, CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BusRouteServiceImpl.class);
 
     @Value("${bus.route.challenge.example.file}")
     private String filename;
@@ -21,7 +28,13 @@ public class BusRouteServiceImpl implements BusRouteService, CommandLineRunner {
 
     @Override
     public boolean direct(final Integer depSid, final Integer arrSid) {
-        Paths.get(filename);
-        return true;
+        LOG.debug("direct depSid: [{}], arrSid: [{}]", depSid, arrSid);
+        try {
+            final Stream<String> stream = Files.lines(Paths.get(filename));
+            return true;
+        } catch (IOException e) {
+            LOG.error("Failed to open file", e);
+            throw new RuntimeException("Failed to open file", e);
+        }
     }
 }
