365480c2dfd6db174cade7483979cc16ebaaad24
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2016-10-28 15:49:23 +0300

    Add colors to bash simple test

--- tests/simple_test.sh
+++ tests/simple_test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+echo-red () {
+    echo -e "\e[1;31m$@\e[0m"
+}
+echo-green () {
+    echo -e "\e[1;32m$@\e[0m"
+}
+
 sleep 5
 {
     curl -s "localhost:8088/api/direct?dep_sid=3&arr_sid=4" | grep -E 'true|false' > /dev/null &&
     curl -s "localhost:8088/api/direct?dep_sid=0&arr_sid=1" | grep -E 'true|false' > /dev/null &&
-    echo TEST PASSED!
+    echo-green TEST PASSED!
 } || {
-    echo TEST FAILED!
+    echo-red TEST FAILED!
 }
